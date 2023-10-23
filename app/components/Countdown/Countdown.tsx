939cadc892cd21c6b0857163e9dee4523d1b7fb0
Marcelo <glacial@gmail.com>
2023-10-23 16:43:10 -0400

    remove react-countdown

--- app/components/Countdown/Countdown.tsx
+++ app/components/Countdown/Countdown.tsx
@@ -1,57 +1,52 @@
 'use client'
+
 import { secondaryFont } from '@/util/fonts'
-import Countdown from 'react-countdown'
+import { useCountdown } from '@/hooks/useCountdown'
 
-// @ts-ignore
-const Renderer = ({ hours, days, minutes, seconds, completed }) => {
-    if (completed) {
-        return <></>
-    } else {
-        return (
-            <div className={`grid grid-flow-col gap-2 lg:gap-5 text-center auto-cols-max`}>
-                <div className='flex flex-col p-2 bg-black rounded-box text-neutral-content'>
-                    <span
-                        className={`countdown font-mono text-center text-4xl lg:text-7xl ${secondaryFont.className}`}
-                    >
-                        {/* @ts-ignore */}
-                        <span style={{ '--value': days }}></span>
-                    </span>
-                    dias
-                </div>
-                <div className='flex flex-col p-2 bg-black rounded-box text-neutral-content'>
-                    <span
-                        className={`countdown font-mono text-center text-4xl lg:text-7xl ${secondaryFont.className}`}
-                    >
-                        {/* @ts-ignore */}
-                        <span style={{ '--value': hours }}></span>
-                    </span>
-                    horas
-                </div>
-                <div className='flex flex-col p-2 bg-black rounded-box text-neutral-content'>
-                    <span
-                        className={`countdown font-mono text-center text-4xl lg:text-7xl ${secondaryFont.className}`}
-                    >
-                        {/* @ts-ignore */}
-                        <span style={{ '--value': minutes }}></span>
-                    </span>
-                    min
-                </div>
-                <div className='flex flex-col p-2 bg-black rounded-box text-neutral-content'>
-                    <span
-                        className={`countdown font-mono text-center text-4xl lg:text-7xl ${secondaryFont.className}`}
-                    >
-                        {/* @ts-ignore */}
-                        <span style={{ '--value': seconds }}></span>
-                    </span>
-                    seg
-                </div>
+const Countdown: React.FC = () => {
+    const finalDate = new Date('Oct 28 2023 19:00:00 EST')
+    const timeLeft = useCountdown(finalDate)
+
+    return (
+        <div className={`grid grid-flow-col gap-2 lg:gap-5 text-center auto-cols-max`}>
+            <div className='flex flex-col p-2 bg-black rounded-box text-neutral-content'>
+                <span
+                    className={`countdown font-mono text-center text-4xl lg:text-7xl ${secondaryFont.className}`}
+                >
+                    {/* @ts-ignore */}
+                    <span style={{ '--value': timeLeft.days }}></span>
+                </span>
+                dias
             </div>
-        )
-    }
+            <div className='flex flex-col p-2 bg-black rounded-box text-neutral-content'>
+                <span
+                    className={`countdown font-mono text-center text-4xl lg:text-7xl ${secondaryFont.className}`}
+                >
+                    {/* @ts-ignore */}
+                    <span style={{ '--value': timeLeft.hours }}></span>
+                </span>
+                horas
+            </div>
+            <div className='flex flex-col p-2 bg-black rounded-box text-neutral-content'>
+                <span
+                    className={`countdown font-mono text-center text-4xl lg:text-7xl ${secondaryFont.className}`}
+                >
+                    {/* @ts-ignore */}
+                    <span style={{ '--value': timeLeft.minutes }}></span>
+                </span>
+                min
+            </div>
+            <div className='flex flex-col p-2 bg-black rounded-box text-neutral-content'>
+                <span
+                    className={`countdown font-mono text-center text-4xl lg:text-7xl ${secondaryFont.className}`}
+                >
+                    {/* @ts-ignore */}
+                    <span style={{ '--value': timeLeft.seconds }}></span>
+                </span>
+                seg
+            </div>
+        </div>
+    )
 }
 
-const CountdownComponent: React.FC = () => {
-    return <Countdown date={new Date('Oct 28 2023 19:00:00 EST')} renderer={Renderer} />
-}
-
-export default CountdownComponent
+export default Countdown
