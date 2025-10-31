496a9dced5d54979f40b2b1f870fe463fbad27a1
Marcelo <glacial@gmail.com>
2025-10-31 17:36:25 -0400

    no botacao

---
+++ app/kjkjh-votacao/categories/[id]/page.tsx
@@ -0,0 +1,50 @@
+import BackButton from "@/components/back-button";
+import { fetchData } from "@/util/fetch-data";
+import VoteGrid from "@/components/votacao/vote-grid";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface CategoryPageParams {
+  id: string;
+}
+
+interface CategoryPageProps {
+  params: CategoryPageParams;
+}
+
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const { id } = await params;
+  const category: Category = await fetchData(`categories/${id}`);
+  const users: User[] = await fetchData("users");
+
+  if (!category) return <>Categoria não encontrada.</>;
+  if (!users) return <>Erro ao carregar os usuários.</>;
+
+  // Get user from cookies
+  const cookieStore = await cookies();
+  const userCookie = cookieStore.get("user");
+  const user: User = userCookie ? JSON.parse(userCookie.value) : null;
+
+  if (!user?.fullName) {
+    redirect("/votacao");
+  }
+
+  // Filter group
+  const isGroup = category.title?.toLowerCase().includes("grupo");
+  const isJunior = category.title?.toLowerCase().includes("j");
+  const allUsers: User[] = users.filter((u) => {
+    if (isGroup) return !!u.group === isGroup;
+    if (isJunior) return !!u.junior === isJunior;
+    return u;
+  });
+
+  return (
+    <section className="min-h-screen w-screen justify-center px-8 py-16 text-center">
+      <BackButton href="/votacao/categories" />
+      <h1 className="mt-8 mb-8 text-4xl lg:text-6xl">
+        {category.icon} {category.title}
+      </h1>
+      <VoteGrid user={user} users={allUsers} categoryId={category._id} />
+    </section>
+  );
+}
---
+++ app/kjkjh-votacao/categories/page.tsx
@@ -0,0 +1,45 @@
+import CategoryList from "@/components/votacao/category-list";
+import { fetchData } from "@/util/fetch-data";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function PoolHome() {
+  const cookieStore = await cookies();
+  const userCookie = cookieStore.get("user");
+  let user = { _id: "", fullName: "", email: "", imageUrl: undefined };
+  if (userCookie?.value) {
+    try {
+      user = JSON.parse(userCookie.value);
+    } catch {}
+  }
+
+  if (!user.fullName) {
+    redirect("/votacao");
+  }
+
+  const categories: Category[] = await fetchData("categories");
+  if (!categories.length) return <p>Nenhuma categoria encontrada.</p>;
+
+  const votes: Vote[] = user._id
+    ? await fetchData(`votes?voterId=${user._id}`)
+    : [];
+  const votedCategoryIds = new Set(
+    votes.map((v) => v.categoryId?.toString?.() || v.categoryId),
+  );
+
+  return (
+    <section className="mmin-h-screen flex w-screen flex-col items-center justify-center gap-8 p-8 text-center">
+      <h1 className="text-5xl">
+        Bem-vindo, <br />
+        {user.fullName}!
+      </h1>
+      <p className="mt-2 text-xl text-orange-400 lg:text-2xl">
+        ATENÇÃO: Você pode votar na mesma pessoa em mais de uma categoria.
+      </p>
+      <CategoryList
+        categories={categories}
+        votedCategoryIds={votedCategoryIds}
+      />
+    </section>
+  );
+}
